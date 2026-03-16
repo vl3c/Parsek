f5283de7c4cd5ab49e615e05276665ff848d6e9a
Vlad Ciobanu <vlad3ciobanu@google.com>
2026-03-16 21:54:08 +0200

    Remove pointless try/finally wrappers from Tier3CExtractedTests

--- Source/Parsek.Tests/Tier3CExtractedTests.cs
+++ Source/Parsek.Tests/Tier3CExtractedTests.cs
@@ -88,197 +88,125 @@ namespace Parsek.Tests
         [Fact]
         public void BuildStateMap_NullNodes_ReturnsEmptyMap()
         {
-            try
-            {
-                var result = MilestoneStore.BuildStateMap(null);
-                Assert.NotNull(result);
-                Assert.Empty(result);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(null);
+            Assert.NotNull(result);
+            Assert.Empty(result);
         }
 
         [Fact]
         public void BuildStateMap_EmptyArray_ReturnsEmptyMap()
         {
-            try
-            {
-                var result = MilestoneStore.BuildStateMap(new ConfigNode[0]);
-                Assert.NotNull(result);
-                Assert.Empty(result);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(new ConfigNode[0]);
+            Assert.NotNull(result);
+            Assert.Empty(result);
         }
 
         [Fact]
         public void BuildStateMap_SingleValidNode_ParsesCorrectly()
         {
-            try
-            {
-                var node = new ConfigNode("MILESTONE_STATE");
-                node.AddValue("id", "abc123");
-                node.AddValue("lastReplayedIdx", "5");
+            var node = new ConfigNode("MILESTONE_STATE");
+            node.AddValue("id", "abc123");
+            node.AddValue("lastReplayedIdx", "5");
 
-                var result = MilestoneStore.BuildStateMap(new[] { node });
-                Assert.Single(result);
-                Assert.Equal(5, result["abc123"]);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(new[] { node });
+            Assert.Single(result);
+            Assert.Equal(5, result["abc123"]);
         }
 
         [Fact]
         public void BuildStateMap_MultipleNodes_ParsesAll()
         {
-            try
-            {
-                var node1 = new ConfigNode("MILESTONE_STATE");
-                node1.AddValue("id", "ms1");
-                node1.AddValue("lastReplayedIdx", "3");
+            var node1 = new ConfigNode("MILESTONE_STATE");
+            node1.AddValue("id", "ms1");
+            node1.AddValue("lastReplayedIdx", "3");
 
-                var node2 = new ConfigNode("MILESTONE_STATE");
-                node2.AddValue("id", "ms2");
-                node2.AddValue("lastReplayedIdx", "7");
+            var node2 = new ConfigNode("MILESTONE_STATE");
+            node2.AddValue("id", "ms2");
+            node2.AddValue("lastReplayedIdx", "7");
 
-                var result = MilestoneStore.BuildStateMap(new[] { node1, node2 });
-                Assert.Equal(2, result.Count);
-                Assert.Equal(3, result["ms1"]);
-                Assert.Equal(7, result["ms2"]);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(new[] { node1, node2 });
+            Assert.Equal(2, result.Count);
+            Assert.Equal(3, result["ms1"]);
+            Assert.Equal(7, result["ms2"]);
         }
 
         [Fact]
         public void BuildStateMap_MissingId_SkipsEntry()
         {
-            try
-            {
-                var node = new ConfigNode("MILESTONE_STATE");
-                // No "id" value
-                node.AddValue("lastReplayedIdx", "5");
+            var node = new ConfigNode("MILESTONE_STATE");
+            node.AddValue("lastReplayedIdx", "5");
 
-                var result = MilestoneStore.BuildStateMap(new[] { node });
-                Assert.Empty(result);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(new[] { node });
+            Assert.Empty(result);
         }
 
         [Fact]
         public void BuildStateMap_MissingIdx_SkipsEntry()
         {
-            try
-            {
-                var node = new ConfigNode("MILESTONE_STATE");
-                node.AddValue("id", "abc123");
-                // No "lastReplayedIdx" value
+            var node = new ConfigNode("MILESTONE_STATE");
+            node.AddValue("id", "abc123");
 
-                var result = MilestoneStore.BuildStateMap(new[] { node });
-                Assert.Empty(result);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(new[] { node });
+            Assert.Empty(result);
         }
 
         [Fact]
         public void BuildStateMap_InvalidIdxFormat_SkipsEntry()
         {
-            try
-            {
-                var node = new ConfigNode("MILESTONE_STATE");
-                node.AddValue("id", "abc123");
-                node.AddValue("lastReplayedIdx", "not_a_number");
+            var node = new ConfigNode("MILESTONE_STATE");
+            node.AddValue("id", "abc123");
+            node.AddValue("lastReplayedIdx", "not_a_number");
 
-                var result = MilestoneStore.BuildStateMap(new[] { node });
-                Assert.Empty(result);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(new[] { node });
+            Assert.Empty(result);
         }
 
         [Fact]
         public void BuildStateMap_NegativeIdx_ParsesCorrectly()
         {
-            try
-            {
-                var node = new ConfigNode("MILESTONE_STATE");
-                node.AddValue("id", "abc123");
-                node.AddValue("lastReplayedIdx", "-1");
+            var node = new ConfigNode("MILESTONE_STATE");
+            node.AddValue("id", "abc123");
+            node.AddValue("lastReplayedIdx", "-1");
 
-                var result = MilestoneStore.BuildStateMap(new[] { node });
-                Assert.Single(result);
-                Assert.Equal(-1, result["abc123"]);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(new[] { node });
+            Assert.Single(result);
+            Assert.Equal(-1, result["abc123"]);
         }
 
         [Fact]
         public void BuildStateMap_LogsEntryCount()
         {
-            try
-            {
-                var node1 = new ConfigNode("MILESTONE_STATE");
-                node1.AddValue("id", "ms1");
-                node1.AddValue("lastReplayedIdx", "2");
+            var node1 = new ConfigNode("MILESTONE_STATE");
+            node1.AddValue("id", "ms1");
+            node1.AddValue("lastReplayedIdx", "2");
 
-                var node2 = new ConfigNode("MILESTONE_STATE");
-                node2.AddValue("id", "ms2");
-                node2.AddValue("lastReplayedIdx", "4");
+            var node2 = new ConfigNode("MILESTONE_STATE");
+            node2.AddValue("id", "ms2");
+            node2.AddValue("lastReplayedIdx", "4");
 
-                MilestoneStore.BuildStateMap(new[] { node1, node2 });
+            MilestoneStore.BuildStateMap(new[] { node1, node2 });
 
-                Assert.Contains(logLines,
-                    l => l.Contains("[MilestoneStore]") && l.Contains("parsed 2 entries") && l.Contains("2 MILESTONE_STATE"));
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            Assert.Contains(logLines,
+                l => l.Contains("[MilestoneStore]") && l.Contains("parsed 2 entries") && l.Contains("2 MILESTONE_STATE"));
         }
 
         [Fact]
         public void BuildStateMap_MixedValidAndInvalid_ParsesOnlyValid()
         {
-            try
-            {
-                var valid = new ConfigNode("MILESTONE_STATE");
-                valid.AddValue("id", "good");
-                valid.AddValue("lastReplayedIdx", "10");
+            var valid = new ConfigNode("MILESTONE_STATE");
+            valid.AddValue("id", "good");
+            valid.AddValue("lastReplayedIdx", "10");
 
-                var badIdx = new ConfigNode("MILESTONE_STATE");
-                badIdx.AddValue("id", "bad1");
-                badIdx.AddValue("lastReplayedIdx", "xyz");
+            var badIdx = new ConfigNode("MILESTONE_STATE");
+            badIdx.AddValue("id", "bad1");
+            badIdx.AddValue("lastReplayedIdx", "xyz");
 
-                var missingId = new ConfigNode("MILESTONE_STATE");
-                missingId.AddValue("lastReplayedIdx", "5");
+            var missingId = new ConfigNode("MILESTONE_STATE");
+            missingId.AddValue("lastReplayedIdx", "5");
 
-                var result = MilestoneStore.BuildStateMap(new[] { valid, badIdx, missingId });
-                Assert.Single(result);
-                Assert.Equal(10, result["good"]);
-            }
-            finally
-            {
-                // cleanup handled by IDisposable.Dispose
-            }
+            var result = MilestoneStore.BuildStateMap(new[] { valid, badIdx, missingId });
+            Assert.Single(result);
+            Assert.Equal(10, result["good"]);
         }
 
         #endregion
