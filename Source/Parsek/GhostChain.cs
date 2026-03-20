e6cecaab0edd895b6ca21c06ccd66eccb89a30f7
Vlad Ciobanu <vlad3ciobanu@google.com>
2026-03-20 19:17:40 +0200

    Add GhostChain data model for chain-based ghost spawning
    ChainLink struct tracks individual recording interactions (merge/split/background event)
    with recording ID, tree ID, branch point, UT, and interaction type. GhostChain class
    holds the full chain state: original vessel PID, ordered links, spawn timing, tip
    recording/tree, and termination flag. Seven unit tests cover defaults, ToString
    formatting, link addition, and field assignment.

---
+++ Source/Parsek/GhostChain.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Parsek
+{
+    internal struct ChainLink
+    {
+        public string recordingId;
+        public string treeId;
+        public string branchPointId;
+        public double ut;
+        public string interactionType;
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture,
+                "ChainLink rec={0} tree={1} type={2} ut={3:F1}",
+                recordingId, treeId, interactionType, ut);
+        }
+    }
+
+    internal class GhostChain
+    {
+        public uint OriginalVesselPid;
+        public List<ChainLink> Links;
+        public double GhostStartUT;
+        public double SpawnUT;
+        public string TipRecordingId;
+        public string TipTreeId;
+        public bool IsTerminated;
+
+        public GhostChain()
+        {
+            Links = new List<ChainLink>();
+        }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture,
+                "GhostChain vessel={0} links={1} tip={2} spawnUT={3:F1} terminated={4}",
+                OriginalVesselPid, Links.Count, TipRecordingId, SpawnUT, IsTerminated);
+        }
+    }
+}
