093ff2b125dd3ee961703cbf65342c8c4c096d14
Vlad Ciobanu <95963142+vl3c@users.noreply.github.com>
2026-08-19 23:29:23 +0300

    Revert "TEMPORARY: plant un-allowlisted ERS/ELS token to prove the gate reds on Linux CI"
    This reverts commit 851e3e85100fdfc8639a482cf9a7dcb64269daa0.

--- Source/Parsek/FileIOUtils.cs
+++ Source/Parsek/FileIOUtils.cs
@@ -1,4 +1,3 @@
-// CI-CANARY (temporary, will be reverted): RecordingStore.CommittedRecordings raw-read token to prove the ERS/ELS grep-audit gate can fail on the Linux Actions runner.
 using System;
 using System.Collections.Generic;
 using System.IO;
